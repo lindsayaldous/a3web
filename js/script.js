ce905cafb4ad5f9ac7d7aa3dd09a38828ee8a0e7
Snehafleming <92972245+Snehafleming@users.noreply.github.com>
2021-12-05 18:14:50 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,3 +56,24 @@ slides[slideIndex-1].style.display = "block";
 // Holds the "active" style of the dot for as long as its corresponding image is being displayed
 dots[slideIndex-1].className += " active";
 }
+
+// Spatial Analysis
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
